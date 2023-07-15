c96fe5ed918ff53931bd6cc82776f71e8c95b588
fabrikna <fabrikna@gmail.com>
2023-07-15 18:06:46 -0300

    altavehiculo.jsp

--- src/main/java/entities/Vehiculo.java
+++ src/main/java/entities/Vehiculo.java
@@ -1,18 +1,25 @@
 package entities;
 
+/**
+ * @author Usuario
+ *
+ */
 public class Vehiculo {
 
 	private int idVehiculo;
+	private int idTipoVehiculo;
 	private double precioporKm;
 	private String marcayModelo;
 	private String color;
 	private boolean estado;
 	private int anio;
+	private double kilometraje;
+	private int pasajeros;
 	private String matricula;
-	private TipoVehiculo tipoVehiculo;
+	//private TipoVehiculo tipoVehiculo;
 	
-	private int cantPasajeros;
-	private double capacidadMax;
+//	private int cantPasajeros;
+//	private double capacidadMax;
 			
 	public int getIdVehiculo() {
 		return idVehiculo;
@@ -20,6 +27,16 @@ public class Vehiculo {
 	public void setIdVehiculo(int idVehiculo) {
 		this.idVehiculo = idVehiculo;
 	}
+		
+	
+	public int getIdTipoVehiculo() {
+		return idTipoVehiculo;
+	}
+	public void setIdTipoVehiculo(int idTipoVehiculo) {
+		this.idTipoVehiculo = idTipoVehiculo;
+	}
+	
+	
 	public double getPrecioporKm() {
 		return precioporKm;
 	}
@@ -50,6 +67,24 @@ public class Vehiculo {
 	public void setAnio(int anio) {
 		this.anio = anio;
 	}
+	
+		
+	public double getKilometraje() {
+		return kilometraje;
+	}
+	public void setKilometraje(double kilometraje) {
+		this.kilometraje = kilometraje;
+	}
+	
+	
+	public int getPasajeros() {
+		return pasajeros;
+	}
+	public void setPasajeros(int pasajeros) {
+		this.pasajeros = pasajeros;
+	}
+	
+	
 	public String getMatricula() {
 		return matricula;
 	}
@@ -58,27 +93,27 @@ public class Vehiculo {
 	}
 	
 	
-	public TipoVehiculo getTipoVehiculo() {
-		return tipoVehiculo;
-	}
-	public void setTipoVehiculo(TipoVehiculo tipoVehiculo) {
-		this.tipoVehiculo = tipoVehiculo;
-	}
+//	public TipoVehiculo getTipoVehiculo() {
+//		return tipoVehiculo;
+//	}
+//	public void setTipoVehiculo(TipoVehiculo tipoVehiculo) {
+//		this.tipoVehiculo = tipoVehiculo;
+//	}
 	
 	
 	
-	public int getCantPasajeros() {
-		return cantPasajeros;
-	}
-	public void setCantPasajeros(int cantPasajeros) {
-		this.cantPasajeros = cantPasajeros;
-	}
-	public double getCapacidadMax() {
-		return capacidadMax;
-	}
-	public void setCapacidadMax(double capacidadMax) {
-		this.capacidadMax = capacidadMax;
-	}
+//	public int getCantPasajeros() {
+//		return cantPasajeros;
+//	}
+//	public void setCantPasajeros(int cantPasajeros) {
+//		this.cantPasajeros = cantPasajeros;
+//	}
+//	public double getCapacidadMax() {
+//		return capacidadMax;
+//	}
+//	public void setCapacidadMax(double capacidadMax) {
+//		this.capacidadMax = capacidadMax;
+//	}
 	
 //	public double calculaImporte() {
 //		// CalculaPrecio(sobrecargado)
